3bb36fa864ae67f1a98bed3d05c49ac909f2d671
alexsedyeti <alexsedyeti@gmail.com>
2024-11-14 20:07:51 +0500

    feat: add catalog menu

--- src/app/components/Header/NavBar/NavBar.js
+++ src/app/components/Header/NavBar/NavBar.js
@@ -1,30 +1,68 @@
 "use client";
-
+// Core
+import { useState, useRef } from "react";
 import Link from "next/link";
 
 // Style
 import s from "./NavBar.module.css";
 
 function NavBar() {
+  const [showCatalogMenu, setShowCatalogMenu] = useState(false);
+  const timeoutRef = useRef(null);
+
+  const handleMouseEnter = () => {
+    clearTimeout(timeoutRef.current);
+    timeoutRef.current = setTimeout(() => {
+      setShowCatalogMenu(true);
+    }, 100);
+  };
+
+  const handleMouseLeave = () => {
+    clearTimeout(timeoutRef.current);
+    timeoutRef.current = setTimeout(() => {
+      setShowCatalogMenu(false);
+    }, 100);
+  };
+
   return (
     <nav className={s.nav}>
       <ul className={s.list}>
-        <li>
+        <li className={s.listItem}>
           <Link className={s.navLink} href="/">
             Главная
           </Link>
         </li>
-        <li>
+        <li className={s.listItem}>
           <Link className={s.navLink} href="/info">
             О производстве
           </Link>
         </li>
-        <li>
-          <Link className={s.navLink} href="#catalog">
-            Каталог
-          </Link>
+        <li
+          className={`${s.listItem} ${s.catalogLink}`}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          Каталог
+          <ul className={`${s.catalogMenu} ${showCatalogMenu ? s.show : ""}`}>
+            <li className={s.catalogMenuItem}>
+              <Link href="/catalog/vodogreinye-kotly">Водогрейные котлы</Link>
+            </li>
+            <li className={s.catalogMenuItem}>
+              <Link href="/catalog/parovye-kotly">Паровые котлы</Link>
+            </li>
+            <li className={s.catalogMenuItem}>
+              <Link href="/catalog/kotly-na-peregretoj-vode">
+                Котлы на перегретой воде
+              </Link>
+            </li>
+            <li className={s.catalogMenuItem}>
+              <Link href="/catalog/kotly-na-diatermicheskom-masle">
+                Котлы на диатермическом масле
+              </Link>
+            </li>
+          </ul>
         </li>
-        <li>
+        <li className={s.listItem}>
           <Link className={s.navLink} href="/contacts">
             Контакты
           </Link>
